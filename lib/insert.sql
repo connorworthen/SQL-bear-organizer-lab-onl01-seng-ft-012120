91340d9d66599dfdb0189fc57c8aefcc5b59759a
connor worthen <connorw220@protonmail.com>
2020-03-04 22:58:22 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 5, M, brown, happy, Y );
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rowdy", 3, M, black, aggressive, Y );
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Tabitha", 7, F, white, gentle, Y );
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Sergeant Brown", 10, M, brown, protective, Y );
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Melissa", 2, F, black, playful, nice, Y );
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Grinch", 12, M, black, angry, Y );
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Wendy", 8, F, brown, protective, Y );
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("unnamed", 15, M, white, aggressive, N );
