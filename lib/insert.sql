3c5b59b64e828722ebd3e466c8aa5654704db74d
connor worthen <connorw220@protonmail.com>
2020-03-04 23:06:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Mr. Chocolate", 5, M, brown, happy, Y );
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Rowdy", 3, M, black, aggressive, Y );
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Tabitha", 7, F, white, gentle, Y );
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Sergeant Brown", 10, M, brown, protective, Y );
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Melissa", 2, F, black, playful, nice, Y );
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Grinch", 12, M, black, angry, Y );
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Wendy", 8, F, brown, protective, Y );
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES (NULL, 15, M, white, aggressive, N );
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'Male', 'Brown', 'Choleric', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 12, 'Female', 'Brown', 'Melancholic', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'Female', 'Brown', 'Sanguine', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Karen', 20, 'Female', 'Brown', 'Choleric', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Fenton', 7, 'Male', 'Brown', 'Sanguine', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 13, 'Male', 'Brown', 'Choleric', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 10, 'Female', 'Brown', 'Phlegmatic', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 1, 'Male', 'Brown', 'Phlegmatic', 1);
